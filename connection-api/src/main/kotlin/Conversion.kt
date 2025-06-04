e40bf34c11d72b7bf1153f199085403d2f835d13
spacedvoid <114347266+spacedvoid@users.noreply.github.com>
2025-06-04 20:59:44 +0900

    Relax conversion receivers to `Iterable`, replace implementations with builders

--- connection-api/src/main/kotlin/Conversion.kt
+++ connection-api/src/main/kotlin/Conversion.kt
@@ -8,50 +8,57 @@
 
 package io.github.spacedvoid.connection
 
+import io.github.spacedvoid.connection.impl.MutableNavigableMapImpl
+import io.github.spacedvoid.connection.impl.MutableNavigableSetImpl
+import io.github.spacedvoid.connection.impl.NavigableMapImpl
+import io.github.spacedvoid.connection.impl.NavigableSetImpl
 import io.github.spacedvoid.connection.utils.naturalOrdering
 import io.github.spacedvoid.connection.utils.reverseOrdering
+import java.util.TreeMap
 
 /**
  * Returns a [List] converted from this collection.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <T> CollectionView<T>.toList(): List<T> = if(this is List<T>) this else listOf(*toGenericArray())
+fun <T> Iterable<T>.toList(): List<T> = if(this is List<T>) this else buildList { addAll(this@toList) }
 
 /**
  * Returns a [Set] converted from this collection.
  */
-fun <T> CollectionView<T>.toSet(): Set<T> = if(this is Set<T>) this else setOf(*toGenericArray())
+fun <T> Iterable<T>.toSet(): Set<T> = if(this is Set<T>) this else buildSet { addAll(this@toSet) }
 
 /**
  * Returns a [SequencedSet] converted from this collection.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <T> CollectionView<T>.toSequencedSet(): SequencedSet<T> = if(this is SequencedSet<T>) this else sequencedSetOf(*toGenericArray())
+fun <T> Iterable<T>.toSequencedSet(): SequencedSet<T> = if(this is SequencedSet<T>) this else buildSequencedSet { addAll(this@toSequencedSet) }
 
 /**
  * Returns a [NavigableSet] converted from this collection, using the [natural order][naturalOrder].
  *
  * If this set uses a different comparator than the natural order, this set is copied into a new set.
  */
-fun <T: Comparable<T>> CollectionView<T>.toNavigableSet(): NavigableSet<T> =
-	if(this is NavigableSet<T> && this.comparator isApparently naturalOrder<T>()) this else navigableSetOf(*toGenericArray())
+fun <T: Comparable<T>> Iterable<T>.toNavigableSet(): NavigableSet<T> =
+	if(this is NavigableSet<T> && this.comparator isApparently naturalOrder<T>()) this
+	else NavigableSetImpl(MutableNavigableSetImpl<T>(sortedSetOf(naturalOrder())).apply { addAll(this@toNavigableSet) }.kotlin)
 
 /**
  * Returns a [NavigableSet] converted from this collection, using the [comparator].
  *
  * If this set uses a different comparator than the given one, this set is copied into a new set.
  */
-fun <T> CollectionView<T>.toNavigableSet(comparator: Comparator<in T>): NavigableSet<T> =
-	if(this is NavigableSet<T> && this.comparator isApparently comparator) this else navigableSetOf(comparator, *toGenericArray())
+fun <T> Iterable<T>.toNavigableSet(comparator: Comparator<in T>): NavigableSet<T> =
+	if(this is NavigableSet<T> && this.comparator isApparently comparator) this
+	else NavigableSetImpl(MutableNavigableSetImpl<T>(sortedSetOf(comparator)).apply { addAll(this@toNavigableSet) }.kotlin)
 
 /**
  * Returns a [SequencedMap] converted from this map.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <K, V> MapView<K, V>.toSequencedMap(): SequencedMap<K, V> = if(this is SequencedMap<K, V>) this else sequencedMapOf(*toArray())
+fun <K, V> MapView<K, V>.toSequencedMap(): SequencedMap<K, V> = if(this is SequencedMap<K, V>) this else buildSequencedMap { putAll(this@toSequencedMap) }
 
 /**
  * Returns a [NavigableMap] converted from this map, using the [natural order][naturalOrder].
@@ -59,7 +66,8 @@ fun <K, V> MapView<K, V>.toSequencedMap(): SequencedMap<K, V> = if(this is Seque
  * If this map uses a different comparator than the natural order, this map is copied into a new map.
  */
 fun <K: Comparable<K>, V> MapView<K, V>.toNavigableMap(): NavigableMap<K, V> =
-	if(this is NavigableMap<K, V> && this.comparator isApparently naturalOrder<K>()) this else navigableMapOf(*toArray())
+	if(this is NavigableMap<K, V> && this.comparator isApparently naturalOrder<K>()) this
+	else NavigableMapImpl(MutableNavigableMapImpl<K, V>(TreeMap(naturalOrder<K>())).apply { putAll(this@toNavigableMap) }.kotlin)
 
 /**
  * Returns a [NavigableMap] converted from this map, using the [comparator].
@@ -67,72 +75,67 @@ fun <K: Comparable<K>, V> MapView<K, V>.toNavigableMap(): NavigableMap<K, V> =
  * If this map uses a different comparator than the given one, this map is copied into a new map.
  */
 fun <K, V> MapView<K, V>.toNavigableMap(comparator: Comparator<in K>): NavigableMap<K, V> =
-	if(this is NavigableMap<K, V> && this.comparator isApparently comparator) this else navigableMapOf(comparator, *toArray())
+	if(this is NavigableMap<K, V> && this.comparator isApparently comparator) this
+	else NavigableMapImpl(MutableNavigableMapImpl<K, V>(TreeMap(comparator)).apply { putAll(this@toNavigableMap) }.kotlin)
 
 /**
  * Returns a new [MutableList] copied from the collection.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <T> CollectionView<T>.toMutableList(): MutableList<T> = mutableListOf(*toGenericArray())
+fun <T> Iterable<T>.toMutableList(): MutableList<T> = mutableListOf<T>().apply { addAll(this@toMutableList) }
 
 /**
  * Returns a new [MutableSet] copied from the collection.
  */
-fun <T> CollectionView<T>.toMutableSet(): MutableSet<T> = mutableSetOf(*toGenericArray())
+fun <T> Iterable<T>.toMutableSet(): MutableSet<T> = mutableSetOf<T>().apply { addAll(this@toMutableSet) }
 
 /**
  * Returns a new [MutableSequencedSet] copied from the collection.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <T> CollectionView<T>.toMutableSequencedSet(): MutableSequencedSet<T> = mutableSequencedSetOf(*toGenericArray())
+fun <T> Iterable<T>.toMutableSequencedSet(): MutableSequencedSet<T> = mutableSequencedSetOf<T>().apply { addAll(this@toMutableSequencedSet) }
 
 /**
  * Returns a new [MutableNavigableSet] copied from the collection.
  *
  * The [natural order][naturalOrder] is used.
  */
-fun <T: Comparable<T>> CollectionView<T>.toMutableNavigableSet(): MutableNavigableSet<T> = mutableNavigableSetOf(*toGenericArray())
+fun <T: Comparable<T>> Iterable<T>.toMutableNavigableSet(): MutableNavigableSet<T> = mutableNavigableSetOf<T>().apply { addAll(this@toMutableNavigableSet) }
 
 /**
  * Returns a new [MutableNavigableSet] copied from the collection.
  *
  * The given [comparator] is used.
  */
-fun <T> CollectionView<T>.toMutableNavigableSet(comparator: Comparator<in T>): MutableNavigableSet<T> = mutableNavigableSetOf(comparator, *toGenericArray())
+fun <T> Iterable<T>.toMutableNavigableSet(comparator: Comparator<in T>): MutableNavigableSet<T> = mutableNavigableSetOf(comparator).apply { addAll(this@toMutableNavigableSet) }
 
 /**
  * Returns a new [MutableMap] copied from the map.
  */
-fun <K, V> MapView<K, V>.toMutableMap(): MutableMap<K, V> = mutableMapOf(*toArray())
+fun <K, V> MapView<K, V>.toMutableMap(): MutableMap<K, V> = mutableMapOf<K, V>().apply { putAll(this@toMutableMap) }
 
 /**
  * Returns a new [MutableSequencedMap] copied from the map.
  *
  * The iteration order is defined as the encounter order.
  */
-fun <K, V> MapView<K, V>.toMutableSequencedMap(): MutableSequencedMap<K, V> = mutableSequencedMapOf(*toArray())
+fun <K, V> MapView<K, V>.toMutableSequencedMap(): MutableSequencedMap<K, V> = mutableSequencedMapOf<K, V>().apply { putAll(this@toMutableSequencedMap) }
 
 /**
  * Returns a new [MutableNavigableMap] copied from the map.
  *
  * The [natural order][naturalOrder] is used.
  */
-fun <K: Comparable<K>, V> MapView<K, V>.toMutableNavigableMap(): MutableNavigableMap<K, V> = mutableNavigableMapOf(*toArray())
+fun <K: Comparable<K>, V> MapView<K, V>.toMutableNavigableMap(): MutableNavigableMap<K, V> = mutableNavigableMapOf<K, V>().apply { putAll(this@toMutableNavigableMap) }
 
 /**
  * Returns a new [MutableNavigableMap] copied from the collection.
  *
  * The given [comparator] is used.
  */
-fun <K, V> MapView<K, V>.toMutableNavigableMap(comparator: Comparator<in K>): MutableNavigableMap<K, V> = mutableNavigableMapOf(comparator, *toArray())
-
-@Suppress("UNCHECKED_CAST", "KotlinConstantConditions")
-internal fun <T> CollectionView<T>.toGenericArray(): Array<T> {
-	val iterator = this.iterator()
-	return Array<Any?>(size()) { iterator.next() } as Array<T>
-}
+fun <K, V> MapView<K, V>.toMutableNavigableMap(comparator: Comparator<in K>): MutableNavigableMap<K, V> = mutableNavigableMapOf<K, V>(comparator).apply { putAll(this@toMutableNavigableMap) }
 
 private val naturalOrderings: kotlin.collections.Set<Comparator<*>> = kotlin.collections.setOf(
 	java.util.Comparator.naturalOrder<Comparable<Any>>(),
